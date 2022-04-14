f6413429b99c61f726277eb1676ba575ca5a3bfd
Luiser-ttv <58303209+Luiser-ttv@users.noreply.github.com>
2022-04-14 11:52:29 +0200

    World 1-1 finished, enemies must be included

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public enum Direction
+{
+    None,
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+public class Teleporter : MonoBehaviour
+{
+    public bool autoTrigger;
+    public bool staticScreen;
+    public Vector3 spawnPoint;
+    public Vector3 cameraPos;
+    public Direction enterDir;
+    public Direction exitDir;
+
+    private Camera _camera;
+    
+    public void Start() {
+        _camera = Camera.main;
+    }
+    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player") || !(Input.GetButtonDown("Down") || autoTrigger)) return;
+        SoundGuy.Instance.PlaySound("smb_pipe");
+        StartCoroutine(Teleport(other));
+    }
+
+    private IEnumerator Teleport(Collider2D player)
+    {
+        
+        player.GetComponent<Rigidbody2D>().simulated = false;
+
+        switch (enterDir)
+        {
+            case Direction.Down:
+                for (int i = 0; i < 80; i++)
+                {
+                    player.transform.Translate(0,-0.025f, 0);
+                    yield return null;
+                }
+                break;
+            case Direction.Left:
+                for (int i = 0; i < 80; i++)
+                {
+                    player.transform.Translate(0.0125f,0, 0);
+                    yield return null;
+                }
+                break;
+        }
+        player.transform.position = spawnPoint;
+        _camera.transform.position = cameraPos;
+        switch (exitDir)
+        {
+            case Direction.Up:
+                for (int i = 0; i < 80; i++)
+                {
+                    player.transform.Translate(0, 0.025f, 0);
+                    yield return null;
+                }
+                break;
+            case Direction.None:
+                break;
+        }
+        
+        player.GetComponent<Rigidbody2D>().simulated = true;
+        _camera.GetComponent<CameraController>().enabled = !staticScreen;
+    }
+    
+
+}
