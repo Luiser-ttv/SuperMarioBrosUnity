00d79926376fbd663fca1353f5e861693525bda1
Luiser-ttv <58303209+Luiser-ttv@users.noreply.github.com>
2022-04-14 11:54:50 +0200

    Little update for static coins

--- Assets/Scripts/StaticCoin.cs
+++ Assets/Scripts/StaticCoin.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 
 public class StaticCoin : MonoBehaviour
 {
+    private CoinManager CM;
+    private void Awake()
+    {
+        CM = GameObject.FindGameObjectWithTag("GameController").GetComponent<CoinManager>();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
         SoundGuy.Instance.PlaySound("smb_coin");
-        //Todo: Increase score and coin counter
+        CM.AddCoins(1);
         Destroy(gameObject);
     }
 }
